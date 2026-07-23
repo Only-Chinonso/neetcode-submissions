5789dd8d6c4d2654bf0214ed7ce44a0d13c59df0
Only-Chinonso <chireexy@gmail.com>
2026-07-23 15:38:02 +0100

    Add: python-self-parameter - submission-1

---
+++ Python OOP/python-self-parameter/submission-1.py
@@ -0,0 +1,17 @@
+class SuperHero:
+    def __init__(self, name: str, power: str, strength: int):
+        self.name = name
+        self.power = power
+        self.strength = strength
+        self.strength_increase = 15
+    
+    def power_boost(self) -> None:
+        self.strength += self.strength_increase
+        print(f"{self.name}'s strength increased to {self.strength}!")
+
+
+
+# Don't modify the following code
+ironman = SuperHero("Iron Man", "Repulsor Beams", 85)
+
+ironman.power_boost()
