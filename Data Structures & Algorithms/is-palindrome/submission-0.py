29d013bf869afbd40b689e9eb185748aa0e89902
Only-Chinonso <chireexy@gmail.com>
2026-07-23 16:20:42 +0100

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        test = ""
+        for cha in s :
+            test += s
+        if test == s :
+            return True
+        return False
