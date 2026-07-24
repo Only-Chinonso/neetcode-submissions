0da533ab9a091771cf1a5538eec5b0d478aebb3d
Only-Chinonso <chireexy@gmail.com>
2026-07-24 17:20:22 +0100

    Add: two-integer-sum-ii - submission-26

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-26.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for num in numbers:
+            diff = target - num
+            if diff in numbers:
+                return sorted([numbers.index(diff) + 1, numbers.index(num) + 1])
+        return []
