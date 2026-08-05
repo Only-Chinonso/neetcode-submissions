a4ee1aea0fe449e17dae7c13d4fa6be7bb4da9d4
Only-Chinonso <chireexy@gmail.com>
2026-08-05 15:05:00 +0100

    Add: median-of-two-sorted-arrays - submission-5

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-5.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        new_nums = sorted(nums1 + nums2)
+        if len(new_nums) % 2 != 0:
+            m = (len(new_nums) - 1) / 2
+            return (new_nums[m] + new_nums[m + 1]) // 2
+        return new_nums[(len(new_nums) - 1) / 2]
+            
