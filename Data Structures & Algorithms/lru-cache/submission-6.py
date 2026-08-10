aa153d25e4e54f1e7e024cf6ede518061efd0903
Only-Chinonso <chireexy@gmail.com>
2026-08-10 15:50:43 +0100

    Add: lru-cache - submission-6

---
+++ Data Structures & Algorithms/lru-cache/submission-6.py
@@ -0,0 +1,21 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.capacity = capacity
+        self.res = set()
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.res.remove(key)
+            self.res.add(key)
+            return self.cache[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache[key] = value
+        else:
+            if len(self.cache) > self.capacity:
+                del self.cache[list(self.cache)[-1]]
+                self.cache[key] = value
